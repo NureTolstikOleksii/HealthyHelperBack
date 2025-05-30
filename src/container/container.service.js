2eb7abdee54ec59e8035d0a7f4fd7cac40a2168e
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2025-05-30 22:41:39 +0300

    commit

--- src/container/container.service.js
+++ src/container/container.service.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 import admin from '../firebase/firebase.js'; // вже є
 import ExcelJS from 'exceljs';
