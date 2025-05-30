560df5ae784ade3a89666fd407135c9f0c129542
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2025-05-30 23:11:42 +0300

    д

--- README.md
+++
@@ -1,1 +0,0 @@
-# HealthyHelperBack
--- src/middleware/role.middleware.js
+++
@@ -1,8 +0,0 @@
-export const authorizeRoles = (...allowedRoles) => {
-    return (req, res, next) => {
-        if (!req.user || !allowedRoles.includes(req.user.roleId)) {
-            return res.status(403).json({ message: 'Access denied: insufficient rights' });
-        }
-        next();
-    };
-};
